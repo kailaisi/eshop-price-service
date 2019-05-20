f68a54b9f31390a4df0fe0fd6ac38f5abf6532c6
wujinxiang <wujinxiang@tcsl.com.cn>
2019-05-20 17:00:20 +0800

    实时性比较高的数据，走redis+mysql双写。

--- src/main/kotlin/com/kailaisi/eshoppriceservice/EshopPriceServiceApplication.kt
+++ src/main/kotlin/com/kailaisi/eshoppriceservice/EshopPriceServiceApplication.kt
@@ -3,10 +3,23 @@ package com.kailaisi.eshoppriceservice
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient
+import org.springframework.context.annotation.Bean
+import redis.clients.jedis.JedisPool
+import redis.clients.jedis.JedisPoolConfig
 
 @SpringBootApplication
 @EnableEurekaClient
-class EshopPriceServiceApplication
+class EshopPriceServiceApplication{
+    @Bean
+    fun jedis(): JedisPool {
+        val config = JedisPoolConfig()
+        config.maxTotal = 100
+        config.maxIdle = 5
+        config.maxWaitMillis = 1000 * 100
+        config.testOnBorrow = true
+        return JedisPool(config, "localhost", 6379)
+    }
+}
 
 fun main(args: Array<String>) {
     runApplication<EshopPriceServiceApplication>(*args)
--- src/main/kotlin/com/kailaisi/eshoppriceservice/service/impl/ProductPriceServiceImpl.kt
+++ src/main/kotlin/com/kailaisi/eshoppriceservice/service/impl/ProductPriceServiceImpl.kt
@@ -5,6 +5,7 @@ import com.kailaisi.eshoppriceservice.model.ProductPrice
 import com.kailaisi.eshoppriceservice.service.ProductPriceService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
+import redis.clients.jedis.JedisPool
 
 /**
  *
@@ -13,20 +14,31 @@ import org.springframework.stereotype.Service
 class ProductPriceServiceImpl : ProductPriceService {
     @Autowired
     lateinit var productPriceMapper: ProductPriceMapper
+    @Autowired
+    lateinit var jedisPool: JedisPool
 
-    override fun delete(id: Long)=
-        productPriceMapper.delete(id)
-
+    override fun add(productPrice: ProductPrice) {
+        productPriceMapper.add(productPrice)
+        val jedis = jedisPool.resource
+        jedis.del("product_price_${productPrice.productId}")
+    }
 
     override fun update(productPrice: ProductPrice) {
         productPriceMapper.update(productPrice)
+        val jedis = jedisPool.resource
+        jedis.del("product_price_${productPrice.productId}")
+    }
+
+    override fun delete(id: Long) {
+        productPriceMapper.delete(id)
+        val jedis = jedisPool.resource
+        val price = findById(id)
+        jedis.del("product_price_${price.productId}")
     }
 
     override fun findById(id: Long): ProductPrice {
         return productPriceMapper.findById(id)
     }
 
-    override fun add(productPrice: ProductPrice) {
-        productPriceMapper.add(productPrice)
-    }
+
 }
